b1117eaaecd7df78d47d0b7ce606142c3c681bc9
Jose Valim <joseaugustovalim1994@gmail.com>
2024-01-17 17:52:32 -0300

    feat: Add versao com maneira mais pura

--- Topico 14 Trabalhando com Arquivos/src/exercicioproposto/Main.java
+++ Topico 14 Trabalhando com Arquivos/src/exercicioproposto/Main.java
@@ -9,32 +9,32 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
+        String path = "C:\\Users\\JV\\Documents\\Desenvolvimento\\Java-Programacao-Orientada-a-Objetos\\Topico 14 Trabalhando com Arquivos\\src\\exercicioproposto\\in.txt";
+        String inputPath = "C:\\Users\\JV\\Documents\\Desenvolvimento\\Java-Programacao-Orientada-a-Objetos\\Topico 14 Trabalhando com Arquivos\\src\\exercicioproposto\\in.txt";
+        String outputPath = "\\C:\\Users\\JV\\Documents\\Desenvolvimento\\Java-Programacao-Orientada-a-Objetos\\Topico 14 Trabalhando com Arquivos\\src\\exercicioproposto\\out\\summary.txt";
 
-        menu();
-
+        List<Product> products = menu(scanner);
+        writeFile(products, path);
+        List<String> inputLine = readLines(path);
+        writeSummary(inputLine, outputPath);
         scanner.close();
     }
 
-    public static void menu() {
-        Scanner scanner = new Scanner(System.in);
-
-        String path = "C:\\Users\\JV\\Documents\\Desenvolvimento\\Java-Programacao-Orientada-a-Objetos\\Topico 14 Trabalhando com Arquivos\\src\\exercicioproposto\\in.txt";
-
+    public static List<Product> menu(Scanner scanner) {
         System.out.println("Enter quantity of products you need enter:");
         int quantityItemsLaunch = scanner.nextInt();
 
+        scanner.nextLine();
 
         List<Product> products = new ArrayList<>();
-        products = itemsLaunch(quantityItemsLaunch, products);
-        writeFile(products, path);
-        summaryTotal(path);
+        products = itemsLaunch(quantityItemsLaunch, scanner);
+
+        return products;
     }
 
-    public static List<Product> itemsLaunch(int quantityItemsLaunch, List<Product> products) {
-        Scanner scanner = new Scanner(System.in);
-
+    public static List<Product> itemsLaunch(int quantityItemsLaunch, Scanner scanner) {
+        List<Product> products = new ArrayList<>();
         Product product;
-
         try {
             for (int i = 0; i < quantityItemsLaunch; i++) {
                 System.out.println("Enter the name of the product:");
@@ -72,28 +72,45 @@ public class Main {
     }
 
 
-    public static void summaryTotal(String enterPath) {
-        String outPath = "C:\\Users\\JV\\Documents\\Desenvolvimento\\Java-Programacao-Orientada-a-Objetos\\Topico 14 Trabalhando com Arquivos\\src\\exercicioproposto\\out\\summary.txt";
+    public static List<String> readLines(String inputPath) {
+        List<String> lineRegister = new ArrayList<>();
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(enterPath));
-             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outPath))) {
-
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputPath))) {
             String line = bufferedReader.readLine();
             while (line != null) {
-                String[] lineSeperated = line.split(",");
-                String total = productCalculator(lineSeperated[0], lineSeperated[1], lineSeperated[2]);
-                bufferedWriter.write(total);
-                bufferedWriter.newLine();
+                String[] lineSeparated = line.split(",");
+                String lineJoined = lineSeparated[0] + "," + lineSeparated[1] + "," + lineSeparated[2];
+                lineRegister.add(lineJoined);
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
+
+        return lineRegister;
     }
 
-    public static String productCalculator(String name, String price, String quantity) {
-        String total = String.valueOf((Double.parseDouble(price) * Integer.parseInt(quantity)));
+    public static void writeSummary(List<String> inputList, String outputPath) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputPath, true))) {
+            for (String product : inputList) {
+                String[] separatedRegisterInput = product.split(",");
+                String name = separatedRegisterInput[0];
+                Double price = Double.valueOf(separatedRegisterInput[1]);
+                Double quantity = Double.valueOf(separatedRegisterInput[2]);
 
+                String joinedRegisterInput = productCalculator(name, price, quantity);
+
+                bufferedWriter.write(joinedRegisterInput);
+                bufferedWriter.newLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static String productCalculator(String name, double price, double quantity) {
+        String total = String.valueOf(price * quantity);
         return name + "," + total;
     }
 }
