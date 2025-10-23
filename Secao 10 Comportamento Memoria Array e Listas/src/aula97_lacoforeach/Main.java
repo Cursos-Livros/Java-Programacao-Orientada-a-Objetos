546e30ba1756540c89a9ea678264907a9d8b0c2e
Jose Valim <joseaugustovalim1994@gmail.com>
2025-10-23 16:49:30 -0300

    feat: aula 97 Laco foreach

---
+++ Secao 10 Comportamento Memoria Array e Listas/src/aula97_lacoforeach/Main.java
@@ -0,0 +1,11 @@
+package aula97_lacoforeach;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] names = {"Abby", "Alloy", "José", "Olga"};
+
+        for(String obj: names){
+            System.out.println(obj);
+        }
+    }
+}
