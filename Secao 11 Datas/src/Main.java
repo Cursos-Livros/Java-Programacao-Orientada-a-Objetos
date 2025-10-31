583826474c12bc36a451fb473fc7bb0f17294f18
Jose Valim <joseaugustovalim1994@gmail.com>
2025-10-31 20:12:03 -0300

    feat: Instanciando data e hora em Java

--- Secao 11 Datas/src/Main.java
+++ Secao 11 Datas/src/Main.java
@@ -28,9 +28,20 @@ public class Main {
         System.out.println("Parsing Brazilian america Sao Paulo in a London time:" + date5);
 
         // Usando classe DateTimeFormatter - para formatar datas
-        // Cuidado aqui estamos transformados uma data em um formato diferente para o formato Local Date
+        // Cuidado aqui estamos transformando uma data em um formato diferente para o formato Local Date
+        // Aqui temos um formatter que diz ao Localdate como interpretar o texto
+        // Esquece que os padrões que podem ser interpretados estão todos disponíveis na documentação
+
         DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate date6 = LocalDate.parse("30/10/2025", formatter1);
         System.out.println(date6);
+
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDateTime date7 = LocalDateTime.parse("31/10/2025 20:00", formatter2);
+        System.out.println("Data e hora formatadas: " + date7);
+
+        // Converter para horario local - não importa a timezone
+        LocalDateTime date8 = LocalDateTime.of(2025,10,31,20,06);
+        System.out.println("Converter para LocalDateTime através dos elementos separados:" +  date8);
     }
 }
