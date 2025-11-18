f650463f741c6720a1f0b485d36554747db05f88
JAV1994 <jose.valim@escola.pr.gov.br>
2025-11-18 10:52:28 -0300

    feat: Classe worker

---
+++ Secao 13 Composicao/src/aula123_Composicao/entities/Worker.java
@@ -0,0 +1,52 @@
+package aula123_Composicao.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Worker {
+    private String name;
+    private WorkerLevel level;
+    private Double baseSalary;
+    private List<HourContract> contracts = new ArrayList<>();
+
+
+    public Worker(String name, WorkerLevel level, Double baseSalary) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public void addContracts(HourContract contract) {
+        contracts.add(contract);
+    }
+
+    public void removeContracts(HourContract contract) {
+        contracts.remove(contract);
+    }
+
+
+}
